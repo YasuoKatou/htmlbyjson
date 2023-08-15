6da1799e23775fee6a43952ee9e2a2b1abc7ff91
yasuokatou <yasuokatou@gmail.com>
2023-08-15 14:24:33 +0900

    簡単なサンプルを作成

---
+++ htmlByJson/core/htmlByJson.js
@@ -0,0 +1,206 @@
+// alert('hello html by json core !!');
+
+class HtmlByJson {
+
+    #configValues = undefined;
+    #idManager = undefined;
+
+    constructor() {
+        this.#configValues = {
+            css: {default: {layout: {flow: {direction: {horizon: 'HBJ-LAYOUT-H001'
+                                                       ,vertical: 'HBJ-LAYOUT-V001'}}}}}
+        };
+        this.#idManager = new this.#RandomManager();
+    }
+
+    /**
+     * 画面定義ファイルのパスを取得
+     */
+    get configPath() {return './htmlByJson/app/config.json';}
+
+    set config(value) {
+        // TODO クラス変数の値を上書きすること
+    }
+
+    getFlowLayoutStyle(direction) {
+        // alert(JSON.stringify(this.#configValues));
+        return this.#configValues.css.default.layout.flow.direction[direction];
+    }
+
+    /**
+     * スタイルシートを読み込む
+     * @param {*} list 
+     */
+    loadCss(list) {
+        let headTag = document.head;
+        for (let path of list) {
+            let css = document.createElement('link');
+            css.setAttribute('rel', 'stylesheet');
+            css.setAttribute('type', 'text/css');
+            css.setAttribute('href', path);
+            headTag.appendChild(css)
+        }
+    }
+
+    appendStyle(element, attr) {
+        if ('css' in attr) {
+            element.classList.add(attr.css);
+        }
+    }
+
+    /**
+     * ラベルコンポーネントを作成する
+     * @param {*} attr ラベルコンポーネントの属性
+     * @returns pタグ
+     */
+    createBlockLabelElement(attr) {
+        let p = document.createElement('p');
+        p.appendChild(document.createTextNode(attr['text']));
+        return p;
+    }
+
+    createLabelElement(attr) {
+        let label = document.createElement('label');
+        label.appendChild(document.createTextNode(attr['text']));
+        this.appendStyle(label, attr);
+        return label;
+    }
+
+    createDivElement(attr) {
+        let div = document.createElement('div');
+        if ('css' in attr) {
+            div.classList.add(attr.css);
+        }
+        return div;
+    }
+
+    createTextboxElement(attr) {
+        let inp = document.createElement('input');
+        inp.setAttribute("type", "text");
+        if ('placeholder' in inp) {
+            if ('placeholder' in attr) {
+                inp.placeholder = attr['placeholder'];
+            }
+        }
+        return inp;
+    }
+
+    createCheckboxElement(attr) {
+        let id = this.#idManager.idBySequence;
+        // alert(id);
+        let label = this.createLabelElement(attr['label']);
+        label.htmlFor = id;
+        let cbx = document.createElement('input');
+        cbx.setAttribute("type", "checkbox");
+        cbx.id = id;
+        label.prepend(cbx);     // 視覚をラベルの左に追加
+        return label;
+    }
+
+    createGroupboxElement(attr) {
+        let legend = document.createElement('legend');
+        legend.appendChild(document.createTextNode(attr['title']));
+        let fieldset = document.createElement('fieldset');
+        fieldset.appendChild(legend);
+        if ('child' in attr) {
+            this.creaeHTML(fieldset, attr.child);
+        }
+        this.appendStyle(fieldset, attr);
+        return fieldset;
+    }
+
+    creaeHTML(parent, data) {
+        // alert(JSON.stringify(data));
+        // alert(parent);
+        for (let item of data) {
+            if ('block-label' in item) {
+                parent.appendChild(this.createBlockLabelElement(item['block-label']));
+            } else if ('div' in item) {
+                parent.appendChild(this.createDivElement(item['div']));
+            } else if ('textbox' in item) {
+                let wk = item['textbox'];
+                if ('label' in wk) {
+                    parent.appendChild(this.createLabelElement(wk.label));
+                }
+                parent.appendChild(this.createTextboxElement(wk));
+            } else if ('checkbox' in item) {
+                parent.appendChild(this.createCheckboxElement(item['checkbox']));
+            } else if ('flow-layout' in item) {
+                let key = 'flow-layout';
+                let layout = new this.#Layout(this);
+                parent.appendChild(layout.getLayout(key, item[key]));
+            } else if ('groupbox' in item) {
+                parent.appendChild(this.createGroupboxElement(item['groupbox']));
+            }
+        }
+    }
+
+    #RandomManager = class {
+        #MAX_ID_RANDAM_DIGIT = undefined;
+        #MAX_ID_RANDAM_BASE = undefined;
+        #idList = undefined;
+        #idMax = undefined;
+        #PREFIX = undefined;
+        constructor() {
+            this.#MAX_ID_RANDAM_DIGIT = 5;
+            this.#MAX_ID_RANDAM_BASE = (this.#MAX_ID_RANDAM_DIGIT - 1) * 10;
+            this.#idMax = -1;
+            this.#idList = [];
+            this.#PREFIX = 'ID-';
+        }
+
+        get idByRandom() {
+            let intVal = Math.floor(Math.random() * this.#MAX_ID_RANDAM_BASE);
+            if (intVal in this.#idList) {
+                this.#idMax = this.#idMax + 1;
+                intVal = this.#idMax;
+            } else  if (this.#idMax < intVal) {
+                this.#idMax = intVal;
+            }
+            this.#idList.push(intVal);
+            return this.#PREFIX + ('0'.repeat(this.#MAX_ID_RANDAM_DIGIT) + intVal).slice(-this.#MAX_ID_RANDAM_DIGIT);
+        }
+
+        get idBySequence() {
+            this.#idMax = this.#idMax + 1;
+            return this.#PREFIX + ('0'.repeat(this.#MAX_ID_RANDAM_DIGIT) + this.#idMax).slice(-this.#MAX_ID_RANDAM_DIGIT);
+        }
+    };
+
+    #Layout = class  {
+        #core = undefined;
+        constructor(core) {
+            this.#core = core;
+        }
+
+        getLayout(layoutType, attr) {
+            if (layoutType === 'flow-layout') {
+                let element = document.createElement('div');
+                element.classList.add(this.#core.getFlowLayoutStyle(attr['direction']));
+                if ('child' in attr) {
+                    this.#core.creaeHTML(element, attr.child);
+                }
+                return element;
+            }
+        }
+    };
+}
+// alert('start !!');
+
+let _obj = new HtmlByJson();
+// alert(_obj.configPath);
+fetch(_obj.configPath)
+    .then(res => res.json())
+    .then(data => {
+        // alert(JSON.stringify(data));
+        if ('css' in data) {
+            _obj.loadCss(data.css);
+        }
+        if ('config' in data) {
+            _obj.config = data.config;
+        }
+        if ('components' in data) {
+            _obj.creaeHTML(document.body, data.components);
+        }
+    })
+    .catch(reason => alert(reason));
